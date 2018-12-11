2efb72728d8a53615af00fdaaa9332da8cc30200
Sasha Goldberg <sashamaryl@gmail.com>
2018-12-10 23:01:48 -0500

    clean up and 'styling'

--- src/main.js
+++ src/main.js
@@ -1,38 +1,37 @@
 import React from 'react'; 
 
 
-//the individual resources (company, school, non-profit)
-export function ResourceListItem(resourceListItem) {
-    return (
-        <li className='list-item'><a href="#target" /* onClick={mapboxPopup}*/ >{resourceListItem.name}</a></li>
-    )
-}
-
-
 //TO-DO when mapbox hooks up 
-function toggleMapIcons(){ 'toggle(category)' };
+export const toggleMapIcons = () => 'toggle(category)';
 
 //toggles child menu
-function toggleChildMenu(category){
-    var myMenu = document.getElementById(category);
-    myMenu.classList.toggle('show')
+export const toggleChildMenu = (category) => {
+    const myMenu = document.getElementById(category);
+    myMenu.classList.toggle('show');
  };
 
-
-export function ResourceSectionHeader(props){
+ export const ResourceSectionHeader = (props) => {
     return (
         <div className='section-header'>
-            <i>{'icon'}</i>
+            <i>{'•'}</i>
             <h2><a href="#target" onClick={() => toggleChildMenu(props.category)}>{props.category}</a></h2>
-            <i>{'chevron'}</i>
+            <i>{'v'}</i>
         </div>
     )
 }
 
-export function ResourceList(props){
+//the individual resources (company, school, non-profit)
+export const ResourceListItem = (resourceListItem) => <li className='list-item'><a href="#target" /* onClick={mapboxPopup}*/ >{resourceListItem.name}</a></li> ;
+
+export const ResourceList = (props) => {
     return(
-        <ul className='child-menu' id={props.category} key={props.category}>
-            {props.resourcesObject.filter(resource => resource.type === props.category).map(resource => <ResourceListItem name={resource.name} key={resource.name} category={resource.type}/>
+        <ul className='child-menu' 
+            id={props.category} 
+            key={props.category}>
+                {props.resourcesObject.filter(resource => resource.type === props.category).map(resource => <ResourceListItem 
+                    name={resource.name} 
+                    key={resource.name} 
+                    category={resource.type}/>
             )}
         </ul>
     )
--- src/map-services.js
+++ src/map-services.js
@@ -1,9 +1,7 @@
-import mapboxgl from 'mapbox-gl';
 
-
-export function getResourceObject(sources){
-  var resourceObject = sources.map( i => {
-      var resource = i.properties; 
+export const getResourceObject = (sources) => {
+  const resourceObject = sources.map( i => {
+      const resource = i.properties; 
       resource.coordinates = i.geometry.coordinates; 
       resource.lngLat = i.lngLat; 
       return resource; 
@@ -12,28 +10,24 @@ export function getResourceObject(sources){
 }
 
 // get the types from mapbox data
-export function getCategories(sources){
-  return sources.map(source => source.properties.type)
-    .filter((value, index, self) => self.indexOf(value) === index ); 
-};
+export const getCategories = (sources) => sources.map(source => source.properties.type)
+    .filter((value, index, self) => self.indexOf(value) === index ) ;
 
-export function makeResourceEntry(resourceObject) {
-  var liLink = document.createElement('a');
-  var liList = document.createElement('li'); 
-  var liContainer = document.createElement('div'); 
+export const makeResourceEntry = (resourceObject) =>{
+  const liLink = document.createElement('a');
+  const liList = document.createElement('li'); 
+  const liContainer = document.createElement('div'); 
   liLink.id = resourceObject.name.toLowerCase().split(" ").join('-'); 
-  var name = resourceObject.name; 
-  var website = resourceObject.website;
-  var bio = resourceObject.bio;
-  var telephone = resourceObject.telephone;
-  var coordinates = resourceObject.coordinates; 
+  const name = resourceObject.name; 
+  const website = resourceObject.website;
+  const bio = resourceObject.bio;
+  const telephone = resourceObject.telephone;
+  const coordinates = resourceObject.coordinates; 
  
-  liLink.onclick = function(e) {
-    new mapboxgl.Popup()
-      .setLngLat(coordinates)
-      .setHTML('<h4>' + name + '</h4><a href=' + website + '>' + website + '</a><br><br><i>' + bio + '</i><br><br><b>Telephone: </b>' + telephone)
-      // .addTo(map);
-  }
+  // liLink.onclick = (e) => new mapboxgl.Popup()
+  //     .setLngLat(coordinates)
+  //     .setHTML('<h4>' + name + '</h4><a href=' + website + '>' + website + '</a><br><br><i>' + bio + '</i><br><br><b>Telephone: </b>' + telephone)
+  //     // .addTo(map);
 
   liContainer.id = resourceObject.name; 
   liLink.innerHTML = resourceObject.name;
@@ -46,3 +40,5 @@ export function makeResourceEntry(resourceObject) {
 
   return liList; 
 }
+
+
