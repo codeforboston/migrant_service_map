06ffd3e058c1a8aa6c2a3acc987eafd976015ba0
rnowrose <rnowrose91@gmail.com>
2019-01-15 19:55:23 -0500

    Add files via upload

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
 import DropdownMenu from "./DropdownMenu";
 import DropdownMenuItem from "./DropdownMenuItem";
+import Search from "./Search";
+import Filter from "./Filter";
 
 export default class Menu extends Component {
-
-
-  
   render() {
-    const { providers, serviceTypes } = this.props;
+    const {providerValue, providers, serviceTypes } = this.props;
 
-    const filteredProvidersList = (type) => providers.filter(provider => provider.properties.type === type) ; 
+    const filteredProvidersList = (type) => providers.filter(provider => provider.properties.type === type) ;
 
     return (
       <div className="side-menu">
+        <DropdownMenu text="Filter" {...this.props}>
+          <Filter/>
+        </DropdownMenu>
+
         <DropdownMenu text="Service Type" {...this.props}>
           {serviceTypes.map((serviceType, index) => (
             <DropdownMenu text={serviceType} key={index}
@@ -36,6 +39,12 @@ export default class Menu extends Component {
           />
         </DropdownMenu>
         <DropdownMenu text="Visa Status" {...this.props}/>
+        <Search
+          providers={providers}
+          SearchProvider={this.props.SearchProvider}
+          providerValue={providerValue}
+        />
+
       </div>
     );
   }
