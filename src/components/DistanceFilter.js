ae3ee0caeb8a2ce68ce6d12dace78f02a054e86d
Byron Hinebaugh <byron@nevada.fiefdom>
2019-02-05 12:14:51 -0500

    got radio buttons working as controlled component, and moved selected state out of component

--- src/components/DistanceFilter.js
+++ src/components/DistanceFilter.js
@@ -1,19 +1,13 @@
 import React, { Component } from "react";
-import mapboxgl from "mapbox-gl";
-import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
-import * as turf from "@turf/turf";
 
 export default class DistanceFilter extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      distanceVisible: []
-    };
   }
  
 
   render() {
-    let { filterDistance, clearFilter } = this.props; 
+    let { clearFilter, changeFilterDistance, distanceVisible } = this.props; 
     const distances = [1, 2, 5];
     return (
       <ul>
@@ -25,10 +19,10 @@ export default class DistanceFilter extends Component {
                 name={"distance"}
                 id={el}
                 value={el}
-                onChange={e => filterDistance(e)}
-                checked={this.state.distanceVisible === el}
+                onChange={e => changeFilterDistance(e)}
+                checked={ distanceVisible.toString() === el.toString() }
               />
-            <label htmlFor={el}>{el + " miles"}</label>
+            <label htmlFor={el}>{el} miles</label>
             </div>
           </li>
         ))}
