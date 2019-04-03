aebda981032092c1c05fca83ebfe11a1c16558f1
Michael Sebastian <michael@urbansitter.com>
2019-04-03 12:47:43 -0400

    Moving search into the top nav

--- src/components/TopNav/top-nav.js
+++ src/components/TopNav/top-nav.js
@@ -4,6 +4,9 @@ import "./top-nav.css";
 const TopNav = () => {
   return (
     <nav>
+      <link rel='stylesheet' 
+      href='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v3.1.6/mapbox-gl-geocoder.css'
+      type='text/css' />
       <div className="header-title">
         <a href="https://refugeeswelcomehome.org/">
           <h1>Migrant Service Map</h1>
@@ -20,6 +23,7 @@ const TopNav = () => {
           Top Nav
         </a>
       </div>
+      <div id="nav-search" class="nav-search"></div>
     </nav>
   );
 };
