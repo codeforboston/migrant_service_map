7a8530a9f6839152b788a2a350501f3723c9408f
Byron Hinebaugh <bhinebaugh@gmail.com>
2019-04-23 00:41:52 -0400

    rework selectors to handle filtering and sorting more flexibly

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -3,53 +3,69 @@ import { createSelector } from "reselect";
 
 const getProviderTypesIds = state => state.providerTypes.allIds;
 const getProviderTypesById = state => state.providerTypes.byId;
+const getVisibleProviderTypes = state => state.providerTypes.visible;
 const getProvidersById = state => state.providers.byId;
 const getDistance = state => state.filters.distance;
 const getSavedProvidersIds = state => state.providers.savedProviders;
 const getHighlightedProvidersList = state => state.highlightedProviders;
-// const getSearchCoordinates = state => state.search.history[state.search.currentLocation];
-const getSearchCoordinates = state => state.search.currentLocation ? state.search.history[state.search.currentLocation] : null; // TODO: separate coordinates and searched location
+const getSearchCoordinates = state => state.search.history[state.search.currentLocation];
+// const getSearchCoordinates = state => state.search.currentLocation ? state.search.history[state.search.currentLocation] : null; // TODO: separate coordinates and searched location
 
 export const getProvidersSorted = createSelector(
   [
-    getProviderTypesIds,
     getProviderTypesById,
+    getVisibleProviderTypes,
     getProvidersById,
     getDistance,
     getSearchCoordinates
+    // visa status,
+    // accepting new clients,
+    // sort criterion
   ],
   (
-    providerTypesIds,
     providerTypesById,
+    visibleProviderTypes,
     providersById,
     distance,
     searchCoordinates
   ) => {
-    const sortBy = "DISTANCE"; // TODO: remove after implementing alphabetical sort and tracking sort method in state
-    if (sortBy === "DISTANCE" && searchCoordinates) {
+    const sortMethod = "DISTANCE"; // TODO: remove after implementing alphabetical sort and tracking sort method in state
+
+    // for each provider type that is active, get providers belonging to it that are near the search location
+    // TODO: limit based on new client status and visa type
+    let groupedByProviderType = visibleProviderTypes.map(id => {
+      let providerType = providerTypesById[id];
+      let providers = providerType.providers.map(provId => providersById[provId]);
       const options = { units: "miles" };
-      const refLocation = searchCoordinates.coordinates;
-      const providersList = providerTypesIds.map(typeId => ({
-        id: typeId,
-        name: providerTypesById[typeId].name,
-        providers: getProvidersWithDistance(
-          providerTypesById[typeId].providers, // array of id's
-          providersById,
-          refLocation,
-          options
-        )
-      }));
-      return providersList;
-    } else {
-      const providersList = providerTypesIds.map(typeId => ({
-        id: typeId,
-        name: providerTypesById[typeId].name,
-        providers: providerTypesById[typeId].providers.map(
-          id => providersById[id]
-        )
-      }));
-      return providersList;
+      let providersWithDistances = calculateProviderDistances(
+        providers,
+        searchCoordinates,
+        options
+      )
+      let nearbyProviders = distance ? getProvidersWithinDistance( providersWithDistances, distance ) : providersWithDistances;
+      return {
+        ...providerType,
+        providers: nearbyProviders
+      }
+    });
+
+    // sort and return an array of grouped providers
+    // (for distance and alphabetical, it's a single-object array)
+    switch (sortMethod) {
+      case "DISTANCE":
+        let flattenedProviders = groupedByProviderType.reduce( (result, type) => result.concat(type.providers), [] )
+        let sortedByDistance = sortProvidersByDistance(flattenedProviders)
+        return [{
+          id: "distance-sort",
+          name: "Closest to farthest",
+          providers: sortedByDistance
+        }]
+      case "NAME": // TODO
+      case "PROVIDER_TYPE":
+      default:
+        return groupedByProviderType;
     }
+
   }
 );
 
@@ -82,25 +98,30 @@ export const getHighlightedProviders = createSelector(
   }
 );
 
-function getProvidersWithDistance(
-  providersArray,
-  providersById,
+function calculateProviderDistances(
+  providers,
   refLocation,
   options
 ) {
-  var referencePoint = point(refLocation);
-  var providers = providersArray.map( providerId => {
-    let provider = providersById[providerId];
+  var referencePoint = point(refLocation.coordinates);
+  return providers.map( provider => {
     // New object with the distance attached
     return {
       ...provider,
       distance: distance(point(provider.coordinates), referencePoint, options)
     }
   })
-  return sortByDistance(providers)
 }
 
-function sortByDistance(providerArray) {
+
+function getProvidersWithinDistance(
+  providers,
+  maxDistance,
+) {
+  return providers.filter( provider => maxDistance && provider.distance < maxDistance )
+}
+
+function sortProvidersByDistance(providerArray) {
     // Sort the list by distance
     return providerArray.sort(
       (a, b) => a.distance - b.distance
