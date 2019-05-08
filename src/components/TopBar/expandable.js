b0ebc15524c9291177bd4018c07d511cbcc3876b
psomers <petersomers2@gmail.com>
2019-05-07 21:48:54 -0400

    prettier

--- src/components/TopBar/expandable.js
+++ src/components/TopBar/expandable.js
@@ -20,7 +20,9 @@ export default class Expandable extends React.Component {
             expanded ? "expanded" : ""
           }`}
         >
-          <div onClick={this.toggleExpanded} className="expandable-header">{header}</div>
+          <div onClick={this.toggleExpanded} className="expandable-header">
+            {header}
+          </div>
           <div className={`expanded-content ${expanded ? "expanded" : ""}`}>
             {content}
           </div>
