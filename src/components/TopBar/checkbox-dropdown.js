0a8011495dd4523f86276e4461a8cf9f3962f797
psomers <petersomers2@gmail.com>
2019-05-21 21:31:30 -0400

    Implementing onChange in checkbox-dropdown, returning an array of checked values replacing fake "options" in visa-status-dropdown with more realistic fake options (they just need to be strings), and implementing subheader

--- src/components/TopBar/checkbox-dropdown.js
+++ src/components/TopBar/checkbox-dropdown.js
@@ -2,30 +2,48 @@ import React from "react";
 import Expandable from "./expandable";
 
 export default class CheckBoxDropdown extends React.Component {
+  optionsMappings = {};
+
+  constructor(props) {
+    super(props);
+    const { options = [] } = props;
+    options.forEach(option => (this.optionsMappings[option] = false));
+  }
+
   render() {
-    const { className, options, header, visibleTypes, handleChange = () => {} } = this.props;
+    const {
+      className,
+      options,
+      header,
+      //visibleTypes
+      onChange = () => {}
+    } = this.props;
     const inputDiv = options.map((value, index) => {
       // content={options.map(({ label, value }, index) => (
-      // const isChecked = visibleTypes.indexOf(value) > -1 ; 
-       return ( <div className="checkbox-container" key={index}>
-            <input
+      // const isChecked = visibleTypes.indexOf(value) > -1 ;
+      return (
+        <div className="checkbox-container" key={index}>
+          <input
             id={value}
             // checked={ visibleTypes.indexOf(value) > -1 }
             type="checkbox"
             value={value}
-            onChange={() => handleChange(value)}
+            onChange={({ target: { checked } }) => {
+              this.optionsMappings[value] = checked;
+              onChange(
+                options.filter(option => this.optionsMappings[option])
+              );
+            }}
           />
-          <label className="expandable-label" htmlFor={value}>{value.toString()}</label>
-          </div>
-         
-      )});
+          <label className="expandable-label" htmlFor={value}>
+            {value.toString()}
+          </label>
+        </div>
+      );
+    });
 
     return (
-      <Expandable
-        className={className}
-        header={header}
-        content={inputDiv}
-      />
+      <Expandable className={className} header={header} content={inputDiv} />
     );
   }
 }
--- src/components/TopBar/visa-status-dropdown.js
+++ src/components/TopBar/visa-status-dropdown.js
@@ -1,8 +1,19 @@
 import React from "react";
 import CheckBoxDropdown from "./checkbox-dropdown";
 
+const defaultVisaStatusValue = "Current Visa Status";
 export default class VisaStatusDropdown extends React.Component {
-  state = { visaStatusValue: "Current Visa Status" };
+  state = { visaStatusValue: defaultVisaStatusValue };
+
+  onCheckboxChanged = selectedValues => {
+    if (selectedValues.length === 0) {
+      this.setState({ visaStatusValue: defaultVisaStatusValue });
+    } else if (selectedValues.length === 1) {
+      this.setState({ visaStatusValue: selectedValues[0] });
+    } else {
+      this.setState({ visaStatusValue: selectedValues.length + " Selected" });
+    }
+  };
 
   render() {
     const { className, onChange = () => {} } = this.props;
@@ -10,11 +21,12 @@ export default class VisaStatusDropdown extends React.Component {
     return (
       <CheckBoxDropdown
         className={className}
-        options={[{ label: <icon type={"cat"} />, value: "item1" }, {}]}
+        options={["visa status 1", "visa status 2"]}
+        onChange={this.onCheckboxChanged}
         header={
           <>
             <h2>VISA STATUS</h2>
-            <p>Current Visa Status</p>
+            <p>{visaStatusValue}</p>
           </>
         }
       />
