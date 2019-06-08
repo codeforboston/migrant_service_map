65015f37f6c551d22e9bddd9eae40f8597969b4c
psomers <petersomers2@gmail.com>
2019-06-08 18:44:28 -0400

    centering the "x" for clearing the distance filter

--- src/components/TopBar/distance-dropdown.js
+++ src/components/TopBar/distance-dropdown.js
@@ -44,7 +44,7 @@ export default class DistanceDropdown extends React.Component {
         options={options}
         header={
           <>
-            <Row alignItems="stretch">
+            <Row alignItems="center">
               <h2 style={{ flex: 1 }}>Distance</h2>
               <div onClick={this.clearDistance}>x</div>
             </Row>
