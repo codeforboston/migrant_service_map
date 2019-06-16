60fabf7d53d974b4ab3cf081fb01d48564d1adb6
Matthew Dailey <mdailey77@gmail.com>
2019-06-16 17:08:31 -0400

    removed toggleHighlight from onClick, added onItemClick

--- src/components/MenuDropdownItem/menu-dropdown-item.js
+++ src/components/MenuDropdownItem/menu-dropdown-item.js
@@ -52,8 +52,7 @@ export default class DropdownMenuItem extends React.Component {
       provider,
       isSaved,
       toggleSavedStatus,
-      isHighlighted,
-      toggleHighlight
+      isHighlighted
     } = this.props;
     const inSavedMenu = this.props.inSavedMenu ? this.props.inSavedMenu : false;
     const savedMenuHighlightedProviderCard =
@@ -67,7 +66,7 @@ export default class DropdownMenuItem extends React.Component {
       >
         <div className="card-container">
           <div className="card-header">
-            <h5 className={expand} onClick={toggleHighlight}>{provider.name}</h5>
+            <h5 className={expand} onClick={this.onItemClick}>{provider.name}</h5>
             <div className="wrapped-info">
               <div className={`prov-type ${expand}`}>
                 <FontAwesomeIcon icon={cardIcon} color={providerTypeToColor[provider.typeName]} />
