7616e9adf9eaea6257ea40e54d48d99ea02b04b0
psomers <petersomers2@gmail.com>
2019-07-02 21:14:22 -0400

    running prettier

--- src/components/MenuDropdown/menu-dropdown.js
+++ src/components/MenuDropdown/menu-dropdown.js
@@ -2,15 +2,10 @@ import React from "react";
 import { Row } from "simple-flexbox";
 import "./menu-dropdown.css";
 
-export default function MenuDropdown({
-  text,
-  children
-}) {
+export default function MenuDropdown({ text, children }) {
   return (
     <>
-      <Row
-        className="dropdown-menu"
-      >
+      <Row className="dropdown-menu">
         <span style={{ flexGrow: 1 }}>{text}</span>
       </Row>
       {children}
--- src/redux/search.js
+++ src/redux/search.js
@@ -1,12 +1,12 @@
 import { SET_SEARCH_COORDINATES, SELECT_TAB } from "./actions";
 
 const INITIAL_STATE = {
-  mapCenter: [-71.066954, 42.359947], 
+  mapCenter: [-71.066954, 42.359947],
   coordinates: [-71.066954, 42.359947],
   selectedTabIndex: 0,
   currentLocation: "default", //references item in history object once user submits search string
   history: {
-    "default": {
+    default: {
       coordinates: [-71.066954, 42.359947],
       searchText: "center of Boston"
     }
@@ -22,7 +22,7 @@ export default function search(state = INITIAL_STATE, action) {
         ...state,
         coordinates: action.coordinates,
         currentLocation: action.mapboxId,
-        history: { 
+        history: {
           ...state.history,
           [action.mapboxId]: {
             coordinates: action.coordinates,
