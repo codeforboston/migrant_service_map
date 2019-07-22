b3e16beb63338d07afeaeffe531a027dbfe0efe4
psomers <petersomers2@gmail.com>
2019-07-21 20:11:44 -0400

    more data conversion, still doesn't work >:(

--- src/util/google_sheets.js
+++ src/util/google_sheets.js
@@ -1,18 +1,19 @@
 import Papa from "papaparse";
 import { initializeProviders } from "redux/actions";
+import _ from "lodash";
 
 const headersToProviderProperties = {
   "Validated By": "validated_by",
-  "Timestamp": "timestamp",
+  Timestamp: "timestamp",
   "Organization Name": "name",
-  "Website": "website",
+  Website: "website",
   "Type of Service": "type_of_service",
-  "Mission": "mission",
-  "Telephone": "telephone",
-  "Email": "email",
-  "Address": "address",
-  "Longitude": "longitude",
-  "Latitude": "latitude"
+  Mission: "mission",
+  Telephone: "telephone",
+  Email: "email",
+  Address: "address",
+  Longitude: "longitude",
+  Latitude: "latitude"
 };
 
 const headerToProviderProperty = header => {
@@ -22,21 +23,81 @@ const headerToProviderProperty = header => {
   try {
     const [headerText] = header.match(/^[a-zA-Z ]*/);
     return headersToProviderProperties[headerText.trim()];
-  } catch(e) {
+  } catch (e) {
     console.error(e);
   }
 };
 
-const providersLoadCallback = ({ data: providers, errors }) => {
+const convertProviderProperties = ({provider, id}) => {
+  const type_of_service_not_normalized = provider.type_of_service;
+  const type_of_service = type_of_service_not_normalized
+    .toLowerCase()
+    .replace(/ /, "-");
+  return {
+    ..._.pick(provider, [
+      "address",
+      "email",
+      "mission",
+      "name",
+      "telephone",
+      "timestamp",
+      "website"
+    ]),
+    id,
+    coordinates: [
+      parseFloat(provider.longitude),
+      parseFloat(provider.latitude)
+    ],
+    typeName: type_of_service_not_normalized,
+    typeId: type_of_service,
+    "Type of Service": type_of_service_not_normalized
+  };
+};
+
+const providersLoadCallback = ({ data: providersData, errors }) => {
   if (Array.isArray(errors) && errors.length > 0) {
     throw new Error(errors);
   }
 
-  const headers = providers.shift();
+  const headers = providersData.shift();
   const providerProperties = headers.map(headerToProviderProperty);
-  console.log(providerProperties);
+
+  const providerTypes = {
+    byId: {},
+    allIds: []
+  };
+  const providerTypeIds = new Set();
+  const providers = {
+    byId: {},
+    allIds: []
+  };
+
+  _.forEach(providersData, (providerRow, index) => {
+    const provider = convertProviderProperties(
+      {
+        provider: _.zipObject(providerProperties, providerRow),
+        id: index
+      }
+    );
+    const { id, typeId } = provider;
+
+    providerTypeIds.add(typeId);
+    if (providerTypes.byId[typeId]) {
+      providerTypes.byId[typeId].providers.push(id);
+    } else {
+      providerTypes.byId[typeId] = {
+        id: typeId,
+        name: provider.typeName,
+        providers: [id]
+      };
+    }
+
+    providers.byId[id] = provider;
+  });
+  providerTypes.allIds = Array.from(providerTypeIds);
+
   debugger;
-  initializeProviders(providers);
+  initializeProviders(providersData);
 };
 
 const getProvidersFromSheet = async url => {
