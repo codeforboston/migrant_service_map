38e3fd47f66beab0a0aa24e55cb40f26f0e26c8e
Alex Ball <ball.alex.509@gmail.com>
2019-08-30 15:59:20 -0400

    Show tooltips for sort and zoom buttons

--- src/components/ProviderList/sort-dropdown.js
+++ src/components/ProviderList/sort-dropdown.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import ReactTooltip from "react-tooltip";
 import Expandable from "../Dropdowns/expandable";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -7,68 +8,90 @@ import {
   faSortAlphaUp,
   faSortAlphaDown
 } from "@fortawesome/free-solid-svg-icons";
-import {faCompass} from "@fortawesome/free-regular-svg-icons";
+import { faCompass } from "@fortawesome/free-regular-svg-icons";
 import "./sort-dropdown.css";
 
-const getSortIcon = (sortDirection, incomingState) => {
-  if (incomingState === 'Distance') {
-    if (sortDirection === 'asc') {
-      return faSortNumericUp;
+export default class SortDropdown extends React.Component {
+  getSortIcon = (sortDirection, incomingState) => {
+    if (incomingState === "Distance") {
+      if (sortDirection === "asc") {
+        return faSortNumericUp;
+      }
+      return faSortNumericDown;
+    } else {
+      if (sortDirection === "asc") {
+        return faSortAlphaUp;
+      }
+      return faSortAlphaDown;
     }
-    return faSortNumericDown;
-  } else {
-    if (sortDirection === 'asc') {
-      return faSortAlphaUp;
-    }
-    return faSortAlphaDown;
-  }
-};
+  };
 
-const SortDropdown = ({
-  className,
-  handleChange,
-  changeDirection,
-  header,
-  incomingState,
-  options,
-  sortDirection,
-  zoomToFit
-}) => {
-  let inputDiv = options.map((option, index) =>
-    <div
-      className={`radio-container ${option === incomingState ? "selected" : null}`}
-      key={index}
-      onClick={() => handleChange(option)}
-    >
-      <input
-        id={option}
-        type="radio"
-        name={option}
-        value={option}
-        checked={option === incomingState}
-        onChange={() => {}}
-      />
-      <label className="expandable-label" htmlFor={option}>
-        {option.toString()}
-      </label>
-    </div>
+  componentDidUpdate() {
+    ReactTooltip.rebuild();
+  }
+
+  render() {
+    const {
+      className,
+      handleChange,
+      changeDirection,
+      header,
+      incomingState,
+      options,
+      sortDirection,
+      zoomToFit
+    } = this.props;
+
+    let inputDiv = options.map((option, index) => (
+      <div
+        className={`radio-container ${
+          option === incomingState ? "selected" : null
+        }`}
+        key={index}
+        onClick={() => handleChange(option)}
+      >
+        <input
+          id={option}
+          type="radio"
+          name={option}
+          value={option}
+          checked={option === incomingState}
+          onChange={() => {}}
+        />
+        <label className="expandable-label" htmlFor={option}>
+          {option.toString()}
+        </label>
+      </div>
+    ));
+    let wrappedHeader = (
+      <h4>
+        {header}: {incomingState}
+      </h4>
     );
-    let wrappedHeader = <h4>{header}: {incomingState}</h4>;
 
     return (
-    <div className="sort-container">
-      <Expandable
-        className={className}
-        header={wrappedHeader}
-        content={inputDiv}
-        closeOnSelect={true}
-      />
-      <FontAwesomeIcon size="2x" icon={getSortIcon(sortDirection, incomingState)}
-        onClick={() => changeDirection()} className="sort-container-icon"/>
-      <FontAwesomeIcon size="2x" icon={faCompass}
-        onClick={zoomToFit} className="sort-container-icon"/>
-    </div>
-    )
-};
-
-export default SortDropdown;
+      <div className="sort-container">
+        <Expandable
+          className={className}
+          header={wrappedHeader}
+          content={inputDiv}
+          closeOnSelect={true}
+        />
+        <FontAwesomeIcon
+          size="2x"
+          icon={this.getSortIcon(sortDirection, incomingState)}
+          onClick={() => changeDirection()}
+          className="sort-container-icon"
+          data-tip="Change sort direction"
+        />
+        <FontAwesomeIcon
+          size="2x"
+          icon={faCompass}
+          onClick={zoomToFit}
+          className="sort-container-icon"
+          data-tip="Zoom to fit"
+        />
+      </div>
+    );
+  }
+}
