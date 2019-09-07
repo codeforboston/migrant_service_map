6350b6007b0c9afa6a17085f6a47f297d601034a
ericbowman <ericjbowman@gmail.com>
2019-09-07 15:32:20 -0400

    Delete unused code

--- src/components/SavedProvidersList/saved-providers-list.js
+++ src/components/SavedProvidersList/saved-providers-list.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { MenuDropdownItem } from "..";
 import { printJSX } from "util/printJSX";
 import "./saved-providers-list.css";
