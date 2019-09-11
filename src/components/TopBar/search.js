f03e14f374a6ead9ecbe1ab646b368b3c98e1749
Nicholas  Simmons <nick@advencr.com>
2019-09-10 21:42:47 -0400

    updated search box styles to match other topbar-items

--- src/components/TopBar/search.js
+++ src/components/TopBar/search.js
@@ -1,15 +1,11 @@
 import React from "react";
+import "./search.css";
 
 export default class Search extends React.Component {
   render() {
     const { className, onSearchInputClick } = this.props;
     return (
       <>
-        <link
-          rel="stylesheet"
-          href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v3.1.6/mapbox-gl-geocoder.css"
-          type="text/css"
-        />
         <div
           className={className}
           id="nav-search"
