18967c434533e0678ff5fa9a04a016b4a1ba668d
Alex Ball <ball.alex.509@gmail.com>
2019-09-14 17:25:34 -0400

    Fix warnings and make build fail on warning

--- src/components/ProviderList/provider-list.js
+++ src/components/ProviderList/provider-list.js
@@ -16,12 +16,12 @@ class ProviderList extends Component {
     if (newhlp.length && newhlp[0] !== previousProps.highlightedProviders[0]) {
       // CSS 'scroll-behavior: smooth' animates the scroll when scrollTop is updated;
       // adding a delay avoids edge case of scroll-upward not taking 'open' height into account
-      let timeoutID = setTimeout(
-        () => this.listElementRef.current.scrollTop = this.lastHighlightedRef.current.offsetTop,
+      setTimeout(
+        () =>
+          (this.listElementRef.current.scrollTop = this.lastHighlightedRef.current.offsetTop),
         60
-      )
+      );
     }
-
   }
 
   render() {
@@ -66,42 +66,49 @@ class ProviderList extends Component {
               zoomToFit={zoomToFit}
             />
             <ul className="providers-list" ref={this.listElementRef}>
-            {providersList.map(providerType => (
+              {providersList.map(providerType => (
                 <li key={providerType.id}>
                   <MenuDropdown
                     key={providerType.id}
                     id={providerType.id}
                     text={providerType.name}
                   >
-                  <ul className="providers-sublist">
-                  {!!providerType.providers.length &&  //if there is not providers MenuDropdown is not shown
-                    providerType.providers.map(provider => (
-                      <li
-                        key={provider.id}
-                        ref={ provider.id === highlightedProviders[0] ? this.lastHighlightedRef : null }
-                        onClick={() => displayProviderInformation(provider.id)}
-                      >
-                        <MenuDropdownItem
-                          key={provider.id}
-                          provider={provider}
-                          isHighlighted={highlightedProviders.includes(
-                            provider.id
-                          )}
-                          isSaved={
-                            savedProviders.includes(provider.id)
-                              ? "saved"
-                              : "unsaved"
-                          }
-                          toggleSavedStatus={() => saveProvider(provider.id)}
-                          flyToProvider={()=> flyToProvider(provider.id)}
-                        />
-                      </li>
-                    ))
-                  }
-                  </ul>
+                    <ul className="providers-sublist">
+                      {!!providerType.providers.length && //if there is not providers MenuDropdown is not shown
+                        providerType.providers.map(provider => (
+                          <li
+                            key={provider.id}
+                            ref={
+                              provider.id === highlightedProviders[0]
+                                ? this.lastHighlightedRef
+                                : null
+                            }
+                            onClick={() =>
+                              displayProviderInformation(provider.id)
+                            }
+                          >
+                            <MenuDropdownItem
+                              key={provider.id}
+                              provider={provider}
+                              isHighlighted={highlightedProviders.includes(
+                                provider.id
+                              )}
+                              isSaved={
+                                savedProviders.includes(provider.id)
+                                  ? "saved"
+                                  : "unsaved"
+                              }
+                              toggleSavedStatus={() =>
+                                saveProvider(provider.id)
+                              }
+                              flyToProvider={() => flyToProvider(provider.id)}
+                            />
+                          </li>
+                        ))}
+                    </ul>
                   </MenuDropdown>
                 </li>
-            ))}
+              ))}
             </ul>
           </>
         )}
