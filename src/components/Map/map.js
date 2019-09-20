8667013364cf8f0063279a407c624730c2fc6d2e
Alex Ball <ball.alex.509@gmail.com>
2019-09-20 13:24:29 -0400

    Limit clustering to zoom level <15

--- src/components/Map/map.js
+++ src/components/Map/map.js
@@ -218,7 +218,7 @@ class Map extends Component {
           features: []
         },
         cluster: true,
-        clusterMaxZoom: 80, // Max zoom to cluster points on
+        clusterMaxZoom: 14, // Max zoom to cluster points on
         clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
       });
     }
