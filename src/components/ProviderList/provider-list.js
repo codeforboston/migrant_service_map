96493f943a66af11776d56e77025f9307bb6ce36
Byron Hinebaugh <bhinebaugh@example.com>
2019-10-01 18:40:49 -0400

    before scrolling to provider in sidebar, check if its providerType is collapsed; if so, expand it

--- src/components/ProviderList/provider-list.js
+++ src/components/ProviderList/provider-list.js
@@ -21,18 +21,34 @@ class ProviderList extends Component {
     this.setState({ collapsedProviderTypes: updated });
   }
 
+  // if highlightedProviders has changed,
+  // we want to scroll providers list to most recent one
   componentDidUpdate(previousProps) {
-    let newhlp = this.props.highlightedProviders;
-    if (newhlp.length && newhlp[0] !== previousProps.highlightedProviders[0]) {
-      // CSS 'scroll-behavior: smooth' animates the scroll when scrollTop is updated;
-      // adding a delay avoids edge case of scroll-upward not taking 'open' height into account
-      let timeoutID = setTimeout(
-        () => this.listElementRef.current.scrollTop = this.lastHighlightedRef.current.offsetTop,
+    let nowHighlighted = this.props.highlightedProviders;
+    
+    if (nowHighlighted.length && nowHighlighted[0] !== previousProps.highlightedProviders[0]) {
+
+      // expand provider type first if necessary
+      for(let providerType of this.props.providersList) {
+        let providerTypeProviders = providerType.providers.map(p => p.id);
+        let typeIsCollapsed = this.state.collapsedProviderTypes.includes(providerType.id);
+        if (typeIsCollapsed && providerTypeProviders.includes(nowHighlighted[0])) { this.toggleProviderType(providerType.id); }
+      }
+
+      // add a small delay when updating scrollTop to avoid edge case 
+      // of 'open' height not being taken into account 
+      // when scrolling the list upward
+      let timeoutID = setTimeout( () => {
+        this.listElementRef.current.scrollTop = this.lastHighlightedRef.current.offsetTop
+        // NOTE: setting property "scroll-behavior: smooth" in the CSS
+        // lets the browser take care of the animation on scrollTop update
+        },
         60
       )
-    }
 
+    }
   }
+
 
   render() {
     const {
