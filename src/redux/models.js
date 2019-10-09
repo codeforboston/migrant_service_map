e9b0ac7a9b1ef6e92ef628c4dcf21eb3f7ac10dc
Alex Ball <ball.alex.509@gmail.com>
2019-10-09 13:42:38 -0400

    Finish first-pass impl of ProviderBuilder

--- src/redux/models.js
+++ src/redux/models.js
@@ -1,3 +1,29 @@
+import _ from "lodash";
+
+export class InvalidProviderError extends Error {};
+
+/**
+ * List of all provider types recognized by the app and their type ID's.
+ * Provider type names are matched to these values.
+ */
+export const ALL_PROVIDER_TYPES = {
+  jobPlacement: "job-placement",
+  resettlement: "resettlement",
+  health: "health",
+  mentalHealth: "mental-health",
+  legal: "legal",
+  education: "education",
+  communityCenters: "community-centers",
+  cashFoodAssistance: "cash/food-assistance",
+  housing: "housing"
+};
+
+const allTypeIds = Object.values(ALL_PROVIDER_TYPES);
+const typeIdsByNormalizedTypeName = {
+  ..._.zipObject(allTypeIds, allTypeIds),
+  "community-center": ALL_PROVIDER_TYPES.communityCenters
+};
+
 const check = (condition, why, what) => {
   if (!condition) {
     throw Error(`${why}: ${what}`);
@@ -10,47 +36,60 @@ const loadString = (name, string) => {
   return string;
 };
 
-const providerPropertyLoaders = {
-  coordinates: c => {
-    coordinates = Array.of(c);
-    check(coordinates.length === 2, "Expected [lon, lat]", coordinates);
-    check(
-      typeof coordinates[0] === "number" &&
-        coordinates[0] >= -180 &&
-        coordinates[0] <= 180,
-      "Expected [lon, lat]",
-      coordinates
-    );
-    check(
-      typeof coordinates[1] === "number" &&
-        coordinates[1] >= -90 &&
-        coordinates[1] <= 90,
-      "Expected [lon, lat]",
-      coordinates
-    );
-    return coordinates;
-  },
-  address: address => loadString("address", address),
-  email: email => loadString("email", email),
-  name: name => loadString("name", name),
-  mission: mission => loadString("mission", mission),
-  telephone: telephone => loadString("telephone", telephone),
-  website: website => loadString("website", website), // check URL?
-  timestamp: timestamp => loadString("timestamp", timestamp), // check can parse to Date?
-  typeName: typeName => {
-    // check that can resolve to a known type ID, return typeName
-  },
-  typeId: typeName => {
-    // check that can resolve to a known type ID, return id
+const loadTypeId = typeName => {
+  const normalizedTypeName = String(typeName)
+      .toLowerCase()
+      .replace(/ /, "-"),
+    typeId = typeIdsByNormalizedTypeName[normalizedTypeName];
+  if (!typeId) {
+    throw Error(`Could not match type name ${typeName} to type ID`);
   }
+  return typeId;
 };
 
-class ProviderLoader {
+const loadTypeName = typeName => {
+  // The type name is valid if we can use it to load the type id.
+  try {
+    loadTypeId(typeName);
+  } catch (e) {
+    throw Error(`Invalid type name ${typeName}`);
+  }
+  return String(typeName);
+};
+
+const loadCoordinates = c => {
+  coordinates = Array.of(c);
+  check(coordinates.length === 2, "Expected [lon, lat]", coordinates);
+  check(
+    typeof coordinates[0] === "number" &&
+      coordinates[0] >= -180 &&
+      coordinates[0] <= 180,
+    "Expected [lon, lat]",
+    coordinates
+  );
+  check(
+    typeof coordinates[1] === "number" &&
+      coordinates[1] >= -90 &&
+      coordinates[1] <= 90,
+    "Expected [lon, lat]",
+    coordinates
+  );
+  return coordinates;
+};
+
+/**
+ * Validates and creates objects representing service providers and organizes them by type.
+ */
+class ProviderBuilder {
   constructor() {
-    this.currentId = 0;
-    this.loadedProviders = [];
+    this._loadedProviders = [];
   }
 
+  /** 
+   * Adds a provider to the overall set of providers.
+   * 
+   * @throws InvalidProviderError if any of the fields are invalid.
+   */
   addProvider({
     coordinates,
     address,
@@ -63,41 +102,74 @@ class ProviderLoader {
     website = "n/a"
   }) {
     let provider;
-    const load = providerPropertyLoaders;
     try {
       provider = {
-        coordinates: load.coordinates(coordinates),
-        address: load.address(address),
-        email: load.email(email),
-        name: load.name(name),
-        mission: load.mission(mission),
-        telephone: load.telephone(telephone),
-        timestamp: load.timestamp(timestamp),
-        typeName: load.typeName(typeName),
-        website: load.website(website),
-        typeId: load.typeId(typeName)
+        coordinates: loadCoordinates(coordinates),
+        address: loadString("address", address),
+        email: loadString("email", email),
+        name: loadString("name", name),
+        mission: loadString("mission", mission),
+        telephone: loadString("telephone", telephone),
+        website: loadString("website", website), // check URL?
+        timestamp: loadString("timestamp", timestamp), // check can parse to Date?
+        typeName: loadTypeName(typeName),
+        typeId: loadTypeId(typeName)
       };
     } catch (e) {
-      console.warn(`Could not load provider data ${arguments[0]}: ${e}`);
-      return;
+      throw InvalidProviderError(`Invalid provider ${e}`);
     }
-    this.loadedProviders.push(provider);
+    this._loadedProviders.push(provider);
   }
-}
 
-class Provider {
-  constructor({
-    address,
-    color,
-    coordinates,
-    email,
-    id, // Must be unique among providers
-    mission,
-    name,
-    telephone,
-    timestamp,
-    typeId,
-    typeName,
-    website
-  }) {}
+  _getUniqueProviders() {
+    return _.uniqWith(
+      this._loadedProviders,
+      (p1, p2) =>
+        _.isEqual(p1.coordinates, p2.coordinates) && _.isEqual(p1.name, p2.name)
+    );
+  }
+
+  _getProvidersById(providers) {
+    const providersById = {};
+    let id = 0;
+    providers.forEach(provider => {
+      provider.id = id++;
+      providersById[provider.id] = provider;
+    });
+    return providersById;
+  }
+
+  _getProviderTypesByTypeId(providers) {
+    const providersByTypeId = _.groupBy(providers, provider => provider.typeId);
+    const providerTypesByTypeId = {};
+    for (providersOfSameType of Object.values(providersByTypeId)) {
+      const typeId = providersOfSameType[0].typeId;
+      providerTypesByTypeId[typeId] = {
+        id: typeId,
+        // TODO: use a canonical name rather than what's represented in the data.
+        name: providersOfSameType[0].typeName,
+        providers: providersOfSameType.map(provider => provider.id)
+      };
+    }
+    return providerTypesByTypeId;
+  }
+
+  /**
+   * Returns an object with providers and providerTypes, as used to initialize thge app.
+   *
+   * This should be called once, after adding all providers.
+   */
+  build() {
+    const uniqueProviders = this._getUniqueProviders(),
+      providersById = this._getProvidersById(uniqueProviders),
+      providerTypesByTypeId = this._getProviderTypesByTypeId(uniqueProviders);
+
+    return {
+      providers: { byId: providersById },
+      providerTypes: {
+        allIds: Object.keys(providerTypesByTypeId),
+        byId: providerTypesByTypeId
+      }
+    };
+  }
 }
