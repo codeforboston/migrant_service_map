f3532321146b2c262afb32912632b9b8222e6e4f
Aron L <aron@aronlurie.com>
2019-10-21 11:39:02 -0400

    Add functional (e2e) tests

---
+++ cypress/integration/msm/search.spec.js
@@ -0,0 +1,26 @@
+/// <reference types="Cypress" />
+
+context('Search', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('results in concentric circles', () => {
+  	// Search
+	cy.get('.header-subtext')
+		.should('have.text', 'Use the filters in the top bar to adjust the number of results')
+	
+	// Filter
+	cy.get('.mapboxgl-ctrl-geocoder input').type('One Broadway')
+
+	// This might not be necessary but it may help in some circumstances
+	cy.wait(3000)
+
+	// Select a search result
+	cy.get('.suggestions .active a').click()
+
+	// Confirm that rings appear
+	cy.get('.mapboxgl-canvas-container').contains('0.5 mile').should('have.class', 'mapboxgl-marker')
+	cy.get('.mapboxgl-canvas-container').contains('1 mile').should('have.class', 'mapboxgl-marker')
+  });
+})
