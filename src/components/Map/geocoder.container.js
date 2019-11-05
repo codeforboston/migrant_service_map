f45f2222c83c358f0fced7ba63e241c78535f4d1
Alex Ball <ball.alex.509@gmail.com>
2019-11-05 10:31:59 -0500

    Fork map to split out geocoder

---
+++ src/components/Map/geocoder.container.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { connect } from "react-redux";
+import {
+  initializeVisaFilter,
+  setSearchCenterCoordinates,
+  displayProviderInformation,
+  setMapObject,
+  selectTab
+} from "redux/actions";
+import { getMapProviders } from "redux/selectors";
+import Map from "./map";
+
+const MapContainer = props => {
+  return <Map {...props} />;
+};
+
+const mapStateToProps = state => {
+  return {
+    visibleProviders: getMapProviders(state),
+    loadedProviderTypeIds: state.providerTypes.allIds,
+    highlightedProviders: state.highlightedProviders,
+    filters: state.filters,
+    search: state.search
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    initializeVisaFilter: visas => {
+      dispatch(initializeVisaFilter(visas));
+    },
+    displayProviderInformation: id => {
+      dispatch(displayProviderInformation(id));
+    },
+    setMapObject: mapObject => {
+      dispatch(setMapObject(mapObject));
+    },
+    setSearchCenterCoordinates: (coordinates, mapboxId, text) => {
+      dispatch(setSearchCenterCoordinates(coordinates, mapboxId, text));
+    },
+    selectTab: index => {
+      dispatch(selectTab(index));
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MapContainer);
