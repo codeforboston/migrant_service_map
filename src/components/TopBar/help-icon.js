a521e74b59e035f643f8b6ff1820948d2703a4b1
shuh <nonya@biz.com>
2019-11-19 21:16:12 -0500

    got question mark and text in purple w/ Alex's help

--- src/components/TopBar/help-icon.js
+++ src/components/TopBar/help-icon.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+ faQuestionCircle, faAlignCenter
+} from "@fortawesome/free-solid-svg-icons";
+import { Z_BLOCK } from 'zlib';
+import "./top-bar.css";
+
 const topBarItemClass = "top-bar-item";
+
+
 class HelpIcon extends React.Component {
     render() {
-      const { className, onSearchInputClick } = this.props;
+      const { className, onSearchInputClick, questionMark } = this.props;
       return (
-       <>
+       
           <div
           className={className}
             id="nav-help"
-            onClick={onSearchInputClick}
-            
-          />
-          <span>
-          <h2>Help</h2>
-              </span>
-        </>
+            onClick={onSearchInputClick}>
+             <FontAwesomeIcon
+                  id={questionMark}
+                  icon={faQuestionCircle}
+                  color={"Purple"}
+                />
+          <h2 style={faAlignCenter} id={HelpIcon}>HELP</h2>
+          </div>
+    
       );
     }
   } 
