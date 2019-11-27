098370eaa9940a235f82c11078bd94e0ac114dd1
shuh <nonya@biz.com>
2019-11-26 20:24:30 -0500

    trying to style the question mark and help text to center, but nothing i do is working...

--- src/components/TopBar/help-icon.js
+++ src/components/TopBar/help-icon.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
- faQuestionCircle
+ faQuestionCircle, faAlignCenter
 } from "@fortawesome/free-solid-svg-icons";
+import { Z_BLOCK } from 'zlib';
+import "./top-bar.css";
 
 const topBarItemClass = "top-bar-item";
 
 
 class HelpIcon extends React.Component {
     render() {
-      const { className, onSearchInputClick } = this.props;
+      const { className, onSearchInputClick, questionMark } = this.props;
       return (
        
           <div
@@ -17,10 +19,11 @@ class HelpIcon extends React.Component {
             id="nav-help"
             onClick={onSearchInputClick}>
              <FontAwesomeIcon
+                  id={questionMark}
                   icon={faQuestionCircle}
                   color={"Purple"}
                 />
-          <h2 style={{  }}>HELP</h2>
+          <h2 style={faAlignCenter} id={HelpIcon}>HELP</h2>
           </div>
     
       );
