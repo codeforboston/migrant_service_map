75c77689cee038ed1320c3cb34ce5a6ad689dc56
Alex Ball <me@alexjball.com>
2020-08-01 21:48:34 -0400

    Replace broken icon URL's with fontawesome icons.

--- src/components/DetailsPane/provider-details-info.js
+++ src/components/DetailsPane/provider-details-info.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { Column } from "simple-flexbox";
 import "./provider-details-info.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faMapPin, faGlobe, faPhone } from "@fortawesome/free-solid-svg-icons";
 
 export default class ProviderDetailsInfo extends React.Component {
-  onAddressClicked = e => {
+  iconsByType = {
+    "email": faEnvelope,
+    "address": faMapPin,
+    "website": faGlobe,
+    "phone": faPhone,
+  }
+
+  onAddressClicked = (e) => {
     e.stopPropagation();
     if (this.props.onClick) {
       this.props.onClick();
@@ -11,13 +20,13 @@ export default class ProviderDetailsInfo extends React.Component {
   };
 
   render() {
-    const { label, icon, ellipsis } = this.props;
+    const { label, type, ellipsis } = this.props;
     return (
       <span className="provider-details-info">
-        <img
+        <FontAwesomeIcon
+          alt={type + " icon"}
           className="provider-details-icon"
-          alt={icon + " icon"}
-          src={"https://icon.now.sh/" + icon}
+          icon={this.iconsByType[type]}
         />
         <Column
           className={ellipsis ? "ellipsis" : ""}
